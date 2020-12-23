4f4d5fbcedbe917b23fbacaac7f545b998110850
Enol Fernández <enol.fernandez@egi.eu>
2020-12-23 11:46:56 +0000

    dottedsites (#3)
    * Use an array as default for sites
    
    * Support sites with "." in their name
    
    * Add .gitignore
    
    * Fix python formatting

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -4,7 +4,8 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
 def test_vo_files(host):
@@ -13,14 +14,11 @@ def test_vo_files(host):
     assert host.file("/etc/egi/vos/sample_vo/client_secret").exists
     assert host.file("/etc/egi/vos/sample_vo/refresh_token").exists
 
+
 def test_site_files(host):
     endpoint_hash = hashlib.md5(b"https://example.com:5000/v3/").hexdigest()
-    filename = "foo-%s" % endpoint_hash
+    filename = "foo-bar-%s" % endpoint_hash
     assert host.file("/etc/egi/cloud-info/").is_directory
     assert host.file("/etc/egi/cloud-info/%s.yaml" % filename).exists
     assert host.file("/etc/egi/cloud-info/%s.env" % filename).exists
     assert host.file("/etc/cron.d/cloud-info-%s" % filename).exists
-
-
-    
-
